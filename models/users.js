272fa21b2b24cc5e6aaad9e34eefad3e92ba39b6
crewsbd <bdcrews@protonmail.com>
2024-05-22 16:50:08 -0700

    Oauth working

---
+++ models/users.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const validators = require('mongoose-validators');
+
+const userSchema = new Schema({
+    oauthID: {
+        type: Number,
+        required: true,
+        unique: true,
+        index: true,
+        immutable: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: false,
+        unique: true,
+        index: true,
+        immutable: true,
+        validate: validators.isEmail(),
+    },
+    password: {
+        type: String,
+        required: false,
+        unique: false,
+        min: 10,
+        max: 20,
+    },
+    profileUrl: {
+        type: String,
+        required: false,
+        unique: true,
+        validate: validators.isURL(),
+    },
+    authProvider: String,
+});
+
+const Users = model('User', userSchema, 'users');
+
+module.exports = Users;
