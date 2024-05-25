5cc61e1c9ac4d02e801bdc82c53b3079b1658dc5
crewsbd <bdcrews@protonmail.com>
2024-05-25 11:02:27 -0700

    Host update

--- swagger.js
+++ swagger.js
@@ -1,16 +1,16 @@
-const swaggerAutogen = require('swagger-autogen')
+const swaggerAutogen = require('swagger-autogen');
 
 const doc = {
     info: {
         title: 'BookVerse API',
-        description: 'BookVerse API'
+        description: 'BookVerse API',
     },
-    host: 'localhost:3000',
-    schemes: ['https', 'http']
-}
+    host: 'https://bookverse-bzgy.onrender.com/',
+    schemes: ['https', 'http'],
+};
 
-const outputFile = './swagger.json'
-const endPointsFiles = ['./routes/index.js']
+const outputFile = './swagger.json';
+const endPointsFiles = ['./routes/index.js'];
 
 //this will generate the swagger.json
-swaggerAutogen(outputFile, endPointsFiles, doc)
+swaggerAutogen(outputFile, endPointsFiles, doc);
