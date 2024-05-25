c80ebe5f8a72f3e84674ff034d8382e905e1f902
crewsbd <bdcrews@protonmail.com>
2024-05-25 15:22:41 -0700

    Added 404 when books not found

--- controllers/books.js
+++ controllers/books.js
@@ -5,19 +5,44 @@ const getAll = async (req, res) => {
     //#swagger.tags=['books']
     const result = await mongodb.getDatabase().db().collection('book').find();
     result.toArray().then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books);
+        if(books[0]) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(books);
+        }
+        else {
+            res.status(404).json({ message: 'No books found' });
+        }
     });
 };
 
+/**
+ * Get one book
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
 const getSingle = async (req, res) => {
     //#swagger.tags=['books']
     const bookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('book').find({ _id: bookId });
-    result.toArray().then((book) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(book[0]);
-    });
+    const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('book')
+        .find({ _id: bookId });
+    result
+        .toArray()
+        .then((book) => {
+            if(book[0]) {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(book[0]);
+            }
+            else {
+                res.status(404).json({ message: 'Book not found' });
+            }
+ 
+        })
+        .catch((error) => {
+            res.status(404).json({ message: 'User not found' });
+        });
 };
 
 const createBook = async (req, res) => {
@@ -30,13 +55,19 @@ const createBook = async (req, res) => {
         publisher: req.body.publisher,
         publicationDate: req.body.publicationDate,
         genre: req.body.genre,
-        synopsis: req.body.synopsis
+        synopsis: req.body.synopsis,
     };
-    const result = await mongodb.getDatabase().db().collection('book').insertOne(book);
+    const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('book')
+        .insertOne(book);
     if (result.acknowledged) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while inserting the book.');
+        res.status(500).json(
+            response.error || 'Some error occurred while inserting the book.'
+        );
     }
 };
 
@@ -52,25 +83,37 @@ const updateBook = async (req, res) => {
         publisher: req.body.publisher,
         publicationDate: req.body.publicationDate,
         genre: req.body.genre,
-        synopsis: req.body.synopsis
+        synopsis: req.body.synopsis,
     };
-    const response = await mongodb.getDatabase().db().collection('book').replaceOne({ _id: bookId }, book);
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('book')
+        .replaceOne({ _id: bookId }, book);
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the book.');
+        res.status(500).json(
+            response.error || 'Some error occurred while updating the book.'
+        );
     }
 };
 
 const deleteBook = async (req, res) => {
     //#swagger.tags=['Reveiws']
     const bookId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('book').deleteOne({ _id: bookId });
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('book')
+        .deleteOne({ _id: bookId });
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the book.');
+        res.status(500).json(
+            response.error || 'Some error occurred while deleting the book.'
+        );
     }
 };
 
-module.exports = { getAll, getSingle, createBook, updateBook, deleteBook }
+module.exports = { getAll, getSingle, createBook, updateBook, deleteBook };
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ const swaggerRouter = require('./swagger.js');
 // Put all the routers here
 router.use('/api-docs', swaggerRouter);
 router.use('/authentication', authenticationRouter);
-router.use('/users', userRouter);
-router.use('/books', bookRouter);
-router.use('/reviews', reviewRouter);
+//router.use('/users', userRouter);
+//router.use('/books', bookRouter);
+//router.use('/reviews', reviewRouter);
 
 router.use('/books', require('./books.js'));
 
