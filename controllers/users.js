10875dcccac64da80c32db74ee2a515059d0e6fa
crewsbd <bdcrews@protonmail.com>
2024-05-25 15:28:20 -0700

    Added 404 to user crud

--- controllers/users.js
+++ controllers/users.js
@@ -5,34 +5,51 @@ const getAll = async (req, res) => {
     //#swagger.tags=['users']
     const result = await mongodb.getDatabase().db().collection('user').find();
     result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        if (users[0]) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(users);
+        } else {
+            res.status(404).json({ message: 'No users found' });
+        }
     });
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['users']
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('user').find({ _id: userId });
+    const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('user')
+        .find({ _id: userId });
     result.toArray().then((users) => {
+        if (users[0]) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(users[0]); }
+        else {
+            res.status(404).json({message: `User ${req.params.id} not found`})
+        }
     });
 };
 
 const createUser = async (req, res) => {
     //#swagger.tags=['users']
-    // ! Change these dependant on how users are formatted in the database. 
+    // ! Change these dependant on how users are formatted in the database.
     const user = {
         data1: req.body.data1,
-        data2: req.body.data2
-        
+        data2: req.body.data2,
     };
-    const response = await mongodb.getDatabase().db().collection('user').insertOne(user);
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('user')
+        .insertOne(user);
     if (response.acknowledged) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+        res.status(500).json(
+            response.error || 'Some error occurred while updating the user.'
+        );
     }
 };
 
@@ -42,24 +59,36 @@ const updateUser = async (req, res) => {
     // ! Change these dependant on how users are formatted in the database.
     const user = {
         data1: req.body.data1,
-        data2: req.body.data2
+        data2: req.body.data2,
     };
-    const response = await mongodb.getDatabase().db().collection('user').replaceOne({ _id: userID }, user);
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('user')
+        .replaceOne({ _id: userID }, user);
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+        res.status(500).json(
+            response.error || 'Some error occurred while updating the user.'
+        );
     }
 };
 
 const deleteUser = async (req, res) => {
     //#swagger.tags=['users']
     const userID = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('user').deleteOne({ _id: userID});
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('user')
+        .deleteOne({ _id: userID });
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+        res.status(500).json(
+            response.error || 'Some error occurred while updating the user.'
+        );
     }
 };
 
@@ -68,6 +97,6 @@ module.exports = {
     getSingle,
     createUser,
     updateUser,
-    deleteUser
-}
+    deleteUser,
+};
 // testing
